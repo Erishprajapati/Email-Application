776755a36005f6c77f6bd5b81990bb1162bfdd10
scrumbler <irishmjn@gmail.com>
2024-10-03 14:53:32 +0545

    fixed the random password generating issue

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -7,52 +7,53 @@ public class Email {
     private String password;
     private String department;
     private int mailboxCapacity;
+    private int defaultPasswordLength = 10;
     private String alternateEmail;
 
-    //constructor to recieve the first and last name
+    // Constructor to receive the first and last name
     public Email(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        System.out.println("Email has been created " + this.firstName + " " + this.lastName );
+        System.out.println("Email has been created: " + this.firstName + " " + this.lastName );
 
-        //call a method that ask for department and return department
-        this.department = setDepartment(); //department asking to enter the information
-        System.out.println("Department " + this.department);
+        // Call a method that asks for department and returns department
+        this.department = setDepartment(); // Department asking to enter the information
+        System.out.println("Department: " + this.department);
+
+        // Call a method that returns a random password
+        this.password = randomPassword(defaultPasswordLength);
+        System.out.println("Your Password is: " + this.password);
     }
 
-    //ask for the department
+    // Ask for the department
     private String setDepartment(){
-        System.out.print("Department Codes:\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\n Enter Department Code:");
+        System.out.print("Department Codes:\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter Department Code: ");
         Scanner scanner = new Scanner(System.in);
         int depChoice = scanner.nextInt();
         if(depChoice == 1){
-            return"sales";
-        }else if(depChoice == 2){
+            return "Sales";
+        } else if(depChoice == 2){
             return "Development";
-
-        }else if(depChoice == 3){
+        } else if(depChoice == 3){
             return "Accounting";
-        }else{
-            return" ";
+        } else {
+            return "None";
         }
-        //call a method that returns random password
     }
 
-    //generate random password
+    // Generate random password
     private String randomPassword(int length){
         String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%*";
         char[] password = new char[length];
-        for (int i =0; i<length; i++){
-            //generate random number
-          int value =   (int) (Math.random() * passwordSet.length();
-          password[i] = passwordSet.charAt(value);
-
+        for (int i = 0; i < length; i++) {
+            // Generate random number
+            int value = (int) (Math.random() * passwordSet.length());
+            password[i] = passwordSet.charAt(value);
         }
-        return new String (password);
+        return new String(password);
     }
 
-    //set the number of mail that can be sent/received
-    //set the alternate email
-    //change the password
-
+    // Set the number of mails that can be sent/received
+    // Set the alternate email
+    // Change the password
 }
